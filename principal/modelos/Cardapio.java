fcdfd1e478880ee96d5699104c47b3fff89cc0d1
Lucas Huzar <lucashuzar@outlook.com>
2024-04-25 14:59:24 -0300

    Atualização no Cardápio

--- principal/modelos/Cardapio.java
+++ principal/modelos/Cardapio.java
@@ -1,4 +1,46 @@
 package principal.modelos;
 
 public class Cardapio {
+
+    private String comida;
+    private String bebida;
+    private String sobremesa;
+    private String valor;
+
+    public String getComida() {
+        return comida;
+    }
+
+    public void setComida(String comida) {
+        this.comida = comida;
+    }
+
+    public String getBebida() {
+        return bebida;
+    }
+
+    public void setBebida(String bebida) {
+        this.bebida = bebida;
+    }
+
+    public String getSobremesa() {
+        return sobremesa;
+    }
+
+    public void setSobremesa(String sobremesa) {
+        this.sobremesa = sobremesa;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public void cadastrarProduto(){
+
+    }
+
 }
